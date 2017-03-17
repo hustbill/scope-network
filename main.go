3360aa8cdaaebd5dec3bf556308e87b0aa60b9dd
hustbill <billyzhang2010@gmail.com>
2017-03-16 17:06:01 -0700

    setupSocket

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func setupSocket(socketPath string) (net.Listener, error) {
+	os.RemoveAll(filepath.Dir(socketPath))
+	if err := os.MkdirAll(filepath.Dir(socketPath), 0700); err != nil {
+		return nil, fmt.Errorf("failed to create directory %q: %v", filepath.Dir(socketPath), err)
+	}
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen on %q: %v", socketPath, err)
+	}
+
+	log.Printf("Listening on: unix://%s", socketPath)
+	return listener, nil
+}
+
+
+func main() {
+
+}
