c97d534f30ff32caf0e7a302fd83202d936f5ba3
hustbill <billyzhang2010@gmail.com>
2017-03-16 17:02:09 -0700

    add tools shell and go files

---
+++ tools/socks/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"text/template"
+
+	socks5 "github.com/armon/go-socks5"
+	"github.com/weaveworks/docker/pkg/mflag"
+	"github.com/weaveworks/weave/common/mflagext"
+	"golang.org/x/net/context"
+)
+
+
+func main() {
+}
